a1a349a78009e8b3e7e2842a3ab48108798d1fb2
Jonghee Park <jonghee-work@jonghees-imac.korea.ncsoft.corp>
2016-04-07 15:26:07 +0900

    first app test

--- js/work.js
+++ js/work.js
@@ -1,4 +1,23 @@
 (function(){
-    var work = 100;
-    console.log(work);
+    // size controller 
+    var __sideArea, __container, __docW, __docH;
+    
+    function _initialize(){
+        __sideArea = document.querySelectorAll('.side')[0];
+        __container = document.querySelectorAll('.container')[0];
+        
+        window.addEventListener('resize', _setSize);
+        _setSize();
+    }
+    
+    function _setSize(){
+        var doc = document.documentElement;
+        if( __docW == doc.clientWidth && __docH == doc.clientHeight ) return;
+        __docW = doc.clientWidth;
+        __docH = doc.clientHeight;
+        
+        __container.style.width = __docW - 200 - 40 + 'px';
+    }
+    
+    window.addEventListener('DOMContentLoaded', _initialize);
 })();
